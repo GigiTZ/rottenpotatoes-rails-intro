d16eeb12cdf3930e998d6f77698eb67a0ae97d71
Gia Zhuang <gzhuang@uccs.edu>
2019-03-05 06:59:18 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,4 @@
 
 ActiveRecord::Schema.define(version: 20150809022253) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
